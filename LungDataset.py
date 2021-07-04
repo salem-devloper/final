76ca74e26891b064148acdc7e31bb6600c46ef88
salem-devloper <salem.rezzag.39@gmail.com>
2021-07-04 18:32:47 +0100

    refresh folder image

--- LungDataset.py
+++ LungDataset.py
@@ -24,7 +24,7 @@ class LungDataset(Dataset): # inherit from torch.utils.data.Dataset
         self.split = split # train / val / test
         self.transforms = transforms
 
-        self.image_path = self.root_dir + '/COVID/'
+        self.image_path = self.root_dir + '/Viral Pneumonia/'
 
         self.img_size = img_size
 
